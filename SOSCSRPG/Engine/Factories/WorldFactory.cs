ab5a89bc116fa3fa7117a3fff11c10bf3ed6f2cd
Alexander Korn <akorn@gustavus.edu>
2017-10-18 22:50:29 -0500

    First Commit

---
+++ SOSCSRPG/Engine/Factories/WorldFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(-2, -1, "Farmer's Field",
+                "There are rows of corn growing here, with giant rats hiding between them.",
+                "/Engine;component/Models/Images/Locations/FarmFields.png");
+
+            newWorld.AddLocation(-1, -1, "Farmer's House",
+                "This is the house of your neighbor, Farmer Ted.",
+                "/Engine;component/Models/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -1, "Home",
+                "This is your home",
+                "/Engine;component/Models/Images/Locations/Home.png");
+
+            newWorld.AddLocation(-1, 0, "Trading Shop",
+                "The shop of Susan, the trader.",
+                "/Engine;component/Models/Images/Locations/Trader.png");
+
+            newWorld.AddLocation(0, 0, "Town square",
+                "You see a fountain here.",
+                "/Engine;component/Models/Images/Locations/TownSquare.png");
+
+            newWorld.AddLocation(1, 0, "Town Gate",
+                "There is a gate here, protecting the town from giant spiders.",
+                "/Engine;component/Models/Images/Locations/TownGate.png");
+
+            newWorld.AddLocation(2, 0, "Spider Forest",
+                "The trees in this forest are covered with spider webs.",
+                "/Engine;component/Models/Images/Locations/SpiderForest.png");
+
+            newWorld.AddLocation(0, 1, "Herbalist's hut",
+                "You see a small hut, with plants drying from the roof.",
+                "/Engine;component/Models/Images/Locations/HerbalistsHut.png");
+
+            newWorld.AddLocation(0, 2, "Herbalist's garden",
+                "There are many plants here, with snakes hiding behind them.",
+                "/Engine;component/Models/Images/Locations/HerbalistsGarden.pn");
+
+            return newWorld;
+        }
+    }
+}
